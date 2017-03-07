e6f4d77b9728f77efacc79badc87df7e5ff19084
HOOLIGAN <HOOLIGAN@HOOLIGAN>
2017-03-08 01:12:03 +0300

    Add project files.

---
+++ TourClub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TourClub.Startup))]
+namespace TourClub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
